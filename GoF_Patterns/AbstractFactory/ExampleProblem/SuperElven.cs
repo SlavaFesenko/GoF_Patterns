e0335c8808baf8a1a227e466bcf75eb1ae1628e2
Slava Fesenko <fvsleader@gmail.com>
2017-11-23 16:54:47 +0200

    working under abstract factory

---
+++ GoF_Patterns/AbstractFactory/ExampleProblem/SuperElven.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GoF_Patterns.AbstractFactory.ExampleProblem
+{
+    class SuperElven: SuperHero
+    {
+        public override void Hit()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void Move()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ GoF_Patterns/AbstractFactory/ExampleProblem/SuperHero.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GoF_Patterns.AbstractFactory.ExampleProblem
+{
+    abstract class SuperHero
+    {
+        public abstract void Hit();
+        public abstract void Move();
+    }
+}
