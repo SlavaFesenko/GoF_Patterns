f61bc978514aab44104d92e126bec370bf446dec
Slava Fesenko <fvsleader@gmail.com>
2018-01-25 13:39:32 +0200

    test commit dff

--- GoF_Patterns/Program.cs
+++ GoF_Patterns/Program.cs
@@ -7,7 +7,7 @@ namespace GoF_Patterns
     {
         static void Main()
         {
-            PresenterFactory.GetPresenter(PatternType.Bridge).PresentExample();
+            PresenterFactory.GetPresenter(PatternType.Memento).PresentExample();
 
             Console.ReadLine();
         }
